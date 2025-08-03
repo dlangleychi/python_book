3159cdade28fcb554e0b989a107c0bd05d41c8d9
Daniel Langley <dlangley812@hotmail.com>
2025-08-03 18:24:13 -0500

    Do exercises for Variables As Pointers

---
+++ variables_as_pointers/words.py
@@ -0,0 +1,13 @@
+'''
+1. line 1 is same type, size, and same elements; line 2 is same object i.e. same id
+aliases of same object
+
+2.
+in some order {42, 'Mont. ' , ('a', .), range(5,10)}
+
+3.
+The Life of Brian
+
+4.
+[1, 42, 3]
+'''
